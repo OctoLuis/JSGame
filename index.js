255cd7c23873cdd7dd69732a473c0d5320588d7c
OctoLuis <73768287+OctoLuis@users.noreply.github.com>
2022-04-27 17:20:30 -0600

    Updated Map Asset and Position on Canvas

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const canvas = document.querySelector('canvas')
+const context = canvas.getContext('2d')
+
 canvas.width = 1024
 canvas.height = 576
-console.log(canvas)
+
+//context.fillStyle = '#525252'
+context.fillRect(0, 0, canvas.width, canvas.height)
+
+const image = new Image()
+image.src = './images/LoneyTown.png'
+
+image.onload = () => {
+    context.drawImage(image, -450, -600)
+}
